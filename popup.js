25b97edf0815caf79709874f3e70bfe3a59dad19
Arun Marria <arun.marria@gmail.com>
2016-03-08 17:12:52 -0800

    parameters shown in popup

--- popup.js
+++ popup.js
@@ -1,14 +1,14 @@
-// This callback function is called when the content script has been 
+// This callback function is called when the content script has been
 // injected and returned its results
 var rawLink;
-function onPageDetailsReceived(pageDetails)  { 
+function onPageDetailsReceived(pageDetails)  {
 	var str = pageDetails.url;
 	var res = str.replace("github.com", "raw.githubusercontent.com");
 	res = res.replace("blob/", "");
 	pageDetails.url = res;
 	rawLink = res;
-	document.getElementById('url').value = pageDetails.url; 
-} 
+	document.getElementById('url').value = pageDetails.url;
+}
 // Global reference to the status display SPAN
 var statusDisplay = null;
 
@@ -18,8 +18,11 @@ window.addEventListener('load', function(evt) {
     var linkJS = document.getElementById('linkJS');
 	linkJS.addEventListener('click', function() {
 		var whiteSpaceCheck = ($("#whiteSpace").is(':checked')) ? "wt" : "wn";
-		var multipleVarCheck = ($("#multipleVar").is(':checked')) ? "wt" : "wn";
-        var windowVal = "http://samprity.github.io/JSLint1/?Key="+rawLink+","+whiteSpaceCheck+","+multipleVarCheck;
+		var multipleVarCheck = ($("#multipleVar").is(':checked')) ? "mt" : "mn";
+		var bitwiseOperators  = ($("bitWiseOperators").is(':checked')) ? "bt" : "bn";
+		var maxLineLength = $("maxLineLength").val();
+		var maxWarnings = $("maxWarnings").val();
+        var windowVal = "http://samprity.github.io/JSLint1/?Key="+rawLink+","+whiteSpaceCheck+","+multipleVarCheck+","+bitWiseOperators+","+maxLineLength+","+maxWarnings;
 		var win = window.open(windowVal, '_blank');
 	    win.focus();
 }, false);
